ce8cd8eaaab8867d99837ecac918ea396f1cbc8b
Mat Schmid <mathieu.schmid@clearwaterclinical.com>
2017-09-07 20:57:40 -0400

    Created empty Xcode project

---
+++ ParkSpace/ParkSpace/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParkSpace
+//
+//  Created by Mat Schmid on 2017-09-07.
+//  Copyright © 2017 Mat Schmid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
