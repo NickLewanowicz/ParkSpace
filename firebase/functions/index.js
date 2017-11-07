a91194f619dc2b9d565a1fb2c960a3b953ad66eb
KhoiHuynh <khuynh09@hotmail.com>
2017-11-06 19:13:57 -0500

    acesing db

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,16 +1,21 @@
+
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 // Take the text parameter passed to this HTTP endpoint and insert it into the
 // Realtime Database under the path /messages/:pushId/original
-exports.getNearest = functions.database.ref('/users/{pushId}')
-.onWrite(event => {
+exports.getNearest = functions.database.ref('/users/{pushId}/location').onWrite(event =>{
+    //const config = functions.config();
+    //const adminUsersString = config.['access-control-list']['admin-users'];
+    const original = event.data.val();
+    console.log('Uppercasing', event.params.pushId, original);
+    const uppercase = original.toUpperCase();
 
-  console.log('getNearest', event);
-  
-  return event
-});
+    return event.data.ref.parent.child('uppercase').set(uppercase);
+}
+);
+
 
 
 
