269fbd97f9e629cc11ac6ae6c02e3f3b0bcdc55d
KhoiHuynh <khuynh09@hotmail.com>
2017-11-13 23:19:34 -0500

    got a snapshot of all users

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -6,13 +6,11 @@ admin.initializeApp(functions.config().firebase);
 // Take the text parameter passed to this HTTP endpoint and insert it into the
 // Realtime Database under the path /messages/:pushId/original
 exports.getNearest = functions.database.ref('/users/{pushId}/location').onWrite(event =>{
-    //const config = functions.config();
-    //const adminUsersString = config.['access-control-list']['admin-users'];
-    //const original = event.data.val();
-    //console.log('Uppercasing', event.params.pushId, original);
-    //const uppercase = original.toUpperCase();
-    console.log(event);
-    //return event.data.ref.parent.child('uppercase').set(uppercase);
+   //const original = event.data.val(); //will give you the location of the user
+    return admin.database().ref('users/').once('value', (snapshot) => {
+        var snap = snapshot.val(); //this gives you the list of all the users and their childrens        
+        console.log(snap);
+    });
 }
 );
 
