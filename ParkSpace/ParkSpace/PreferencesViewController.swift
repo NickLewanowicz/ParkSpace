baf0217197fdca9b35da1147050ce645f0539c2d
Foopert <foopert@gmail.com>
2017-11-30 16:29:54 -0500

    Added cool new easter egg for real this time

--- ParkSpace/ParkSpace/PreferencesViewController.swift
+++ ParkSpace/ParkSpace/PreferencesViewController.swift
@@ -7,14 +7,19 @@
 //
 
 import UIKit
+import Cheers
 
 class PreferencesViewController: UITableViewController {
     let NUM_SECTIONS = 1
-    let NUM_ROWS_PREFS_SECTION = 1
+    let NUM_ROWS_PREFS_SECTION = 2
     let HEIGHT_FOR_SECTION : CGFloat = 46
-
+    
+    let cheerView = CheerView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupConfetti()
+        
         tableView.register(UINib.init(nibName: "SettingsCellView", bundle: nil), forCellReuseIdentifier: "SettingsCell")
     }
 
@@ -36,20 +41,54 @@ class PreferencesViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cellSwitch = UISwitch(frame: .zero) as UISwitch
-        cellSwitch.isOn = true
-        cellSwitch.addTarget(self, action: #selector(switchTriggered), for: .valueChanged)
-        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsCell", for: indexPath) as! SettingsCell
-        cell.cellLabel.text = "Night Mode"
-        cell.cellImageView.image = #imageLiteral(resourceName: "PSS_NightMode")
-        cell.accessoryView = cellSwitch
-        cell.contentMode = .scaleAspectFit
-        
-        return cell
+        if indexPath.row == 0 {
+            let nightSwitch = UISwitch(frame: .zero) as UISwitch
+            nightSwitch.isOn = true
+            nightSwitch.addTarget(self, action: #selector(nightSwitchTriggered), for: .valueChanged)
+            let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsCell", for: indexPath) as! SettingsCell
+            cell.cellLabel.text = "Night Mode"
+            cell.cellImageView.image = #imageLiteral(resourceName: "PSS_NightMode")
+            cell.accessoryView = nightSwitch
+            cell.contentMode = .scaleAspectFit
+            return cell
+        } else {
+            let coolSwitch = UISwitch(frame: .zero) as UISwitch
+            coolSwitch.isOn = false
+            coolSwitch.addTarget(self, action: #selector(coolSwitchTriggered), for: .valueChanged)
+            let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsCell", for: indexPath) as! SettingsCell
+            cell.cellLabel.text = "Cool Mode"
+            cell.cellImageView.image = #imageLiteral(resourceName: "PSS_FAQ")
+            cell.accessoryView = coolSwitch
+            cell.contentMode = .scaleAspectFit
+            return cell
+        }
     }
     
-    func switchTriggered(sender: UISwitch) {
-        print("switched")
+    func nightSwitchTriggered(sender: UISwitch) {
+        print("night switched")
+    }
+    
+    func coolSwitchTriggered(sender: UISwitch) {
+        print("cool switched")
+        
+        if sender.isOn {
+            startConfetti()
+        } else {
+            stopConfetti()
+        }
+    }
+    
+    func setupConfetti() {
+        view.addSubview(cheerView)
+        cheerView.config.particle = .confetti
+    }
+    
+    func startConfetti() {
+        cheerView.start()
+    }
+    
+    func stopConfetti() {
+        cheerView.stop()
     }
     
 }
